a9c0c7bbedd347ede6a5d1f884566e88e193439d
Sxmourai <samourai_x@outlook.fr>
2022-11-27 13:16:45 +0100

    Installed mongoose Changed architecture, to support ressources/*                                             css / js / img Changed index

--- app/server/server.js
+++ app/server/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const { readFile } = require('fs');
 const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get("/", (req, res) => {
-    res.render("../www/index.html");
+    res.render('../www/index.ejs', { js: '/ressources/js', css: '/ressources/css' });
 });
-app.get("/login", (req, res) => {
-    res.render("../www/login.ejs");
+
+app.get("/ressources/*", (req, res) => {
+    readFile("ressources/" + req.params['0'], (err, data) => {
+        if (err) throw err;
+        res.send(data);
+    });
+    // res.end();
 });
+
 
 app.listen(8008, () => console.log("Server running at http://localhost:8008..."))

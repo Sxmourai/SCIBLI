a9e5ab8623c814d4714637b223493f0731eda527
Sxmourai <samourai_x@outlook.fr>
2022-11-27 14:21:59 +0100

    Added tailwind bootstrap and changed architecture

--- app/server/server.js
+++ app/server/server.js
@@ -1,21 +1,40 @@
 const express = require('express');
-const { readFile } = require('fs');
+const { readFile, readFileSync } = require('fs');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
+
 app.set('view engine', 'ejs')
+app.use(express.static("ressources"));
 
 app.get("/", (req, res) => {
-    res.render('../www/index.ejs', { js: '/ressources/js', css: '/ressources/css' });
+    res.render('../www/index.ejs', { src: "ressources"});
 });
 
-app.get("/ressources/*", (req, res) => {
-    readFile("ressources/" + req.params['0'], (err, data) => {
+// app.get("/ressources/*", (req, res) => {
+//     readFile("ressources/" + req.params['0'], (err, data) => {
+//         if (err) throw err;
+//         res.send(data);
+//     });
+//     // res.end();
+// });
+app.get("/ressources/css/*", (req, res) => {
+    readFileSync("ressources/css/" + req.params['0'], (err, data) => {
         if (err) throw err;
-        res.send(data);
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
     });
-    // res.end();
+    res.end();
+});
+app.get("/ressources/js/*", (req, res) => {
+    readFileSync("ressources/js/" + req.params['0'], (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(data);
+    });
+    res.end();
 });
 
 
---
+++ app/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["www/*", "ressources/js/*", "ressources/css/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
